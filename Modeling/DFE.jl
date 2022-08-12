a5e0a94295ed5af21941a78c51136efb3c0c9e36
BharatRaviIyengar <bharatravi.654@gmail.com>
2022-08-12 12:36:00 +0200

    modified:   Modeling/DFE.jl

--- Modeling/DFE.jl
+++ Modeling/DFE.jl
@@ -1,9 +1,9 @@
 cd(Base.source_dir())
 include("nucleotidefuncts.jl")
 
-blosum = readdlm("BLOSUM62N.csv", ',', Int, '\n'; header = true)
-aasubmat = blosum[1][1:20,1:20]
-aanames = Dict([(blosum[2][i],i) for i in 1:20])
+aasubdata = readdlm("BLOSUM62N.csv", ',', Int, '\n'; header = true)
+aasubmat = aasubdata[1][1:20,1:20]
+aanames = Dict([(aasubdata[2][i],i) for i in 1:20])
 
 allcodons = kmers(3)
 
@@ -20,6 +20,7 @@ r1cdn = reverse.(comp.(f1cdn))
 r2cdn = reverse.(comp.(f2cdn))
 
 gccontent = 0.42
+
 function dfecdn(codon1, codon2)
         mp = nucprob(codon1,gccontent)*mprob(codon1,codon2)
         fe = aasubmat[aanames[transnucs(codon1)],aanames[transnucs(codon2)]]
