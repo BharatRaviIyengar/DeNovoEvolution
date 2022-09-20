2c3fcfdccf162d24dbb14ba47d35697011658cc4
BharatRaviIyengar <bharatravi.654@gmail.com>
2022-09-20 12:45:46 +0200

    modified:   Modeling/DFE.jl

--- Modeling/DFE.jl
+++ Modeling/DFE.jl
@@ -9,19 +9,11 @@ aasubmat[1:20,21] .= -90
 aanames = Dict(vcat([(aasubdata[2][i],i) for i in 1:20],("O",21)))
 allcodons = kmers(3)
 
+aasubmat[1:20,1:20] = (aasubmat[1:20,1:20] + aasubmat[1:20,1:20]')./2
+
 function nostop(codonset)
         codonset[codonset .∉ Ref(["TAA","TAG","TGA"])]
 end
-
-nostopcodons = nostop(allcodons)
-
-f1cdn = permutedims(hcat(frameXcodons.(nostopcodons,1)...))
-f2cdn = permutedims(hcat(frameXcodons.(nostopcodons,2)...))
-
-r1cdn = reverse.(comp.(f1cdn))
-r2cdn = reverse.(comp.(f2cdn))
-
-gccontent = 0.42
 
 function dfecdn(codon1, codon2)
         mp = nucprob(codon1,gccontent)*mprob(codon1,codon2)
@@ -41,21 +33,30 @@ function expdfe(dfeset)
         return sum(dfeset[:,1].*dfeset[:,2])/sum(dfeset[:,2])
 end
 
-dfedist0 = dfedistgen(nostopcodons,nostopcodons)
+function dfeframeX(cdn,frm)
+        fset0 = frameXcodons(cdn,frm)
+        cset1 = singlemutant(cdn)
+        fset1 = hcat([frameXcodons(x,frm) for x in cset1]...)
+        dfe1 = combinedfe(vcat(vcat([[dfecdn(fset0[y],x) for x in fset1[y,:]] for y in 1:20]...)...))
+        return dfe1
+end
 
-dfeXframeshift = zeros(61,4)
+nostopcodons = nostop(allcodons)
+
+f1cdn = permutedims(hcat(frameXcodons.(nostopcodons,1)...))
+f2cdn = permutedims(hcat(frameXcodons.(nostopcodons,2)...))
+
+r1cdn = reverse.(comp.(f1cdn))
+r2cdn = reverse.(comp.(f2cdn))
+
+gccontent = 0.42
+
+dfedist0 = combinedfe(dfedistgen(nostopcodons,nostopcodons))
 
 frames = [-2,-1,1,2]
 
 sdiff2 = (s1,s2) -> s1[s1 .∉ Ref(s2)]
 
-for i in 1:61
-        for j in 1:4
-               cdn = nostopcodons[i]
-               frm = frames[j]
-               fset0 = frameXcodons(cdn,frm)
-               cset1 = singlemutant(cdn)
-               fset1 = hcat([frameXcodons(x,frm) for x in cset1]...)
-               # dfe1 = combinedfe(vcat) edit this
-        end
-end
+dfeXframesehift = hcat([[dfeframeX(x,y) for x in nostopcodons] for y in frames]...)
+
+[expdfe(vcat(dfeXframesehift[:,x]...)) for x in 1:4]
